4034511b58ce2a4be7291e129b8f3b29768bddf9
DheerajaPremdas <dheerajacri@gmail.com>
2024-12-26 12:58:15 +0530

    Add project files.

---
+++ OnlineBookStrore/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineBookStrore.Data;
+using OnlineBookStrore.Models;
+
+namespace OnlineBookStrore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _dbObj;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _dbObj = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _dbObj.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+    }
+}
